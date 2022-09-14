7bf8430cc6424f5224f95d990709c90022cd5f75
UsuarioUsuario@DESKTOP-2NT6HAM <urielalberto75@hotmail.com>
2022-09-14 00:16:28 -0300

    cambio1

--- NuevaHistoriaClinicaBD/Data/Entidades/Paciente.cs
+++ NuevaHistoriaClinicaBD/Data/Entidades/Paciente.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NuevaHistoriaClinicaBD.Data.Entidades
 {
-    internal class Paciente
+    class Paciente
     {
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [MaxLength(30, ErrorMessage = "Este dato no puede superar los {1} caracteres")]
+        public string NombreCompleto { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [MaxLength(8, ErrorMessage = "Este dato no puede superar los {1} caracteres")]
+        public string DNI { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [MaxLength(8, ErrorMessage = "Este dato no puede superar los {1} caracteres")]
+        public int Edad { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(30, ErrorMessage = "Este dato no puede superar los {1} caracteres")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(30, ErrorMessage = "Este dato no puede superar los {1} caracteres")]
+        public string Localidad { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(30, ErrorMessage = "Este dato no puede superar los {1} caracteres")]
+        public int Telefono { get; set; }
+
+
+
     }
+
 }
+
